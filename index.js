acbdf5437466036828ac29f82e0a9b1f5f8cae60
kuronight29 <nightkuro29@gmail.com>
2022-06-06 21:13:10 +0700

    hi

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 
 client.on('ready', () => {
     console.log(`${client.user.tag} đã sẵn sàng!`);
+    client.user.setPresence({ activities: [{ name: 'Bot by Kuro-kun#0965' }], status: 'online' });
 });
+
+client.on('MessageCreate', async (message) => {
+    if (message.author.bot) return;
+    const prefix = '!'; 
+    if (message.content === 'ping') {
+        message.reply('pong');
+    }
+})
 
 client.login(process.env.TOKEN);
