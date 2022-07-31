5b9462afe6cb72ea14ff69af2f58908e6feb96c1
kuronight29 <nightkuro29@gmail.com>
2022-07-31 20:37:59 +0700

    thay đổi lần 1

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-const { Client, Intents } = require('discord.js');
-require('dotenv').config();
+const { Client } = require('discord.js');
+const client = new Client({intents: 3});
+const { } = require("./config.json");
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-client.on('ready', () => {
-    console.log(`${client.user.tag} đã sẵn sàng!`);
-    client.user.setPresence({ activities: [{ name: 'Bot by Kuro-kun#0965' }], status: 'online' });
-});
-
-client.on('MessageCreate', async (message) => {
-    if (message.author.bot) return;
-    const prefix = 'k!'; 
-    if (!message.content.startsWith(prefix)) return;
-    const args = message.content.slice(prefix.length).trim().split(' ');
-    const command = args.shift().toLowerCase();
-    if (command === 'ping') {
-        message.reply(`🏓Pong! \`${client.ws.ping}ms\``);
-    }
-});
-
-client.login(process.env.TOKEN);
+client.login(token);
