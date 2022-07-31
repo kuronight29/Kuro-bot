7e050d38883b1ebb9a7c04a11825ccb160096e20
kuronight29 <nightkuro29@gmail.com>
2022-07-31 20:21:31 +0700

    hi

--- index.js
+++ index.js
@@ -11,9 +11,12 @@ client.on('ready', () => {
 client.on('MessageCreate', async (message) => {
     if (message.author.bot) return;
     const prefix = '!'; 
-    if (message.content === 'ping') {
-        message.reply('pong');
+    if (!message.content.startsWith(prefix)) return;
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+    if (command === 'ping') {
+        message.reply(`🏓Pong! \`${client.ws.ping}ms\``);
     }
-})
+});
 
 client.login(process.env.TOKEN);
